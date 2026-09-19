21f9b5461359cb08451725b4e6b97e5ce3ef6583
Isini Perera <isiniperera08@gmail.com>
2026-09-19 12:22:31 +0530

    Remove author and purpose comments from linkedList.c
    Removed author information and purpose comment from linkedList.c

--- linkedList.c
+++ linkedList.c
@@ -1,8 +1,5 @@
 /*
 File: linkedList.c
-Name: Isini Ayansa Perera
-Student ID: 23601321
-Unit: COMP1000(Unix and C programming)
 Purpose: to implemt the generic linked list to undo
 Last modified: 2026-05-20
 */
