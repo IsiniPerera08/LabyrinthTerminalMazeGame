803b7ce41af9ac46d7baa5bfd88b290af5a1ba8f
Isini Perera <isiniperera08@gmail.com>
2026-09-19 12:26:23 +0530

    Update random.h

--- random.h
+++ random.h
@@ -1,10 +1,3 @@
-/*
-File: random.h
-Name: Isini Ayansa Perera
-Student ID: 23601321
-Unit: COMP1000(Unix and C programming)
-Purpose: declaration of random functions
-*/
 #ifndef RANDOM_H
 #define RANDOM_H
 
