58c1bf12252537670a7a2d58d696e2581bac7ec8
Isini Perera <isiniperera08@gmail.com>
2026-09-19 12:25:16 +0530

    Clean up newSleep.h by removing unnecessary comments
    Removed comments and header information from newSleep.h

--- newSleep.h
+++ newSleep.h
@@ -1,10 +1,3 @@
-/*
-File: newSleep.h
-Name: Isini Ayansa Perera
-Student ID: 23601321
-Unit: COMP1000(Unix and C programming)
-Purpose: Sleeep funtion declared
-*/
 #ifndef NEWSLEEP_H
 #define NEWSLEEP_H
 
