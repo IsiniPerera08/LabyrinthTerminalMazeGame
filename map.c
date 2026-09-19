7914e2637f828edcd9ddd25c6e8c726b834589b3
Isini Perera <isiniperera08@gmail.com>
2026-09-19 12:24:19 +0530

    Remove header comments and clean up map.c

--- map.c
+++ map.c
@@ -1,11 +1,3 @@
-/*
-File: map.c
-Name: Isini Ayansa Perera
-Student ID: 23601321
-Unit: COMP1000(Unix and C programming)
-Purpose: loads, prints the map and frees the map memory
-Last modified: 2026-05-20
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include "color.h"
