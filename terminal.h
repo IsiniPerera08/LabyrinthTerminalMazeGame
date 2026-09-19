2163843c09b88fdd067b14434de65c1a8bdd9f5e
Isini Perera <isiniperera08@gmail.com>
2026-09-19 12:26:54 +0530

    Clean up terminal.h by removing comments
    Removed header comments from terminal.h

--- terminal.h
+++ terminal.h
@@ -1,10 +1,3 @@
-/*
-File: terminal.h
-Name: Isini Ayansa Perera
-Student ID: 23601321
-Unit: COMP1000(Unix and C programming)
-Purpose: declared the terminal functions
-*/
 #ifndef TERMINAL_H
 #define TERMINAL_H
 
